67c04c2726e2bd0b585e61bf31072d7ad12c2049
Justin <jbarnes321@gmail.com>
2013-04-02 20:26:49 -0400

    sean's agenda stuff

--- Agenda/Agenda/Form1.Designer.cs
+++ Agenda/Agenda/Form1.Designer.cs
@@ -29,38 +29,46 @@
         private void InitializeComponent()
         {
             this.EventCreation_GroupBox = new System.Windows.Forms.GroupBox();
-            this.Agenda_GroupBox = new System.Windows.Forms.GroupBox();
-            this.EventNameLabel = new System.Windows.Forms.Label();
-            this.EventNameRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.EventDateRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.EventDateLabel = new System.Windows.Forms.Label();
-            this.EventTimeRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.EventLocationRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.EventLocationLabel = new System.Windows.Forms.Label();
-            this.EventDetailsRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.EventDetailsLabel = new System.Windows.Forms.Label();
+            this.EventNameTextBox = new System.Windows.Forms.TextBox();
             this.CreateButton = new System.Windows.Forms.Button();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.AlarmCheckBox = new System.Windows.Forms.CheckBox();
+            this.EventDetailsLabel = new System.Windows.Forms.Label();
+            this.EventLocationLabel = new System.Windows.Forms.Label();
+            this.EventNameLabel = new System.Windows.Forms.Label();
+            this.EventLocationTextBox = new System.Windows.Forms.TextBox();
+            this.EventDetailsTextBox = new System.Windows.Forms.TextBox();
+            this.AlarmTimeLabel = new System.Windows.Forms.Label();
+            this.AlarmTimeTextBox = new System.Windows.Forms.TextBox();
+            this.MinuteBeforeLabel = new System.Windows.Forms.Label();
+            this.AgendaDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.EventHourTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EventDateLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EventMinTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EventSecondTextBox = new System.Windows.Forms.TextBox();
             this.EventCreation_GroupBox.SuspendLayout();
-            this.Agenda_GroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // EventCreation_GroupBox
             // 
-            this.EventCreation_GroupBox.Controls.Add(this.AlarmCheckBox);
+            this.EventCreation_GroupBox.Controls.Add(this.EventSecondTextBox);
+            this.EventCreation_GroupBox.Controls.Add(this.label2);
+            this.EventCreation_GroupBox.Controls.Add(this.EventMinTextBox);
+            this.EventCreation_GroupBox.Controls.Add(this.label1);
+            this.EventCreation_GroupBox.Controls.Add(this.AgendaDatePicker);
+            this.EventCreation_GroupBox.Controls.Add(this.MinuteBeforeLabel);
+            this.EventCreation_GroupBox.Controls.Add(this.AlarmTimeTextBox);
+            this.EventCreation_GroupBox.Controls.Add(this.AlarmTimeLabel);
+            this.EventCreation_GroupBox.Controls.Add(this.EventDetailsTextBox);
+            this.EventCreation_GroupBox.Controls.Add(this.EventLocationTextBox);
+            this.EventCreation_GroupBox.Controls.Add(this.EventHourTextBox);
+            this.EventCreation_GroupBox.Controls.Add(this.EventNameTextBox);
             this.EventCreation_GroupBox.Controls.Add(this.CreateButton);
-            this.EventCreation_GroupBox.Controls.Add(this.EventDetailsRichTextBox);
             this.EventCreation_GroupBox.Controls.Add(this.EventDetailsLabel);
-            this.EventCreation_GroupBox.Controls.Add(this.EventLocationRichTextBox);
             this.EventCreation_GroupBox.Controls.Add(this.EventLocationLabel);
-            this.EventCreation_GroupBox.Controls.Add(this.EventTimeRichTextBox);
             this.EventCreation_GroupBox.Controls.Add(this.label3);
-            this.EventCreation_GroupBox.Controls.Add(this.EventDateRichTextBox);
             this.EventCreation_GroupBox.Controls.Add(this.EventDateLabel);
-            this.EventCreation_GroupBox.Controls.Add(this.EventNameRichTextBox);
             this.EventCreation_GroupBox.Controls.Add(this.EventNameLabel);
             this.EventCreation_GroupBox.Location = new System.Drawing.Point(4, 4);
             this.EventCreation_GroupBox.Name = "EventCreation_GroupBox";
@@ -68,17 +76,42 @@
             this.EventCreation_GroupBox.TabIndex = 0;
             this.EventCreation_GroupBox.TabStop = false;
             this.EventCreation_GroupBox.Text = "Event Creation";
+            this.EventCreation_GroupBox.Enter += new System.EventHandler(this.EventCreation_GroupBox_Enter);
             // 
-            // Agenda_GroupBox
+            // EventNameTextBox
             // 
-            this.Agenda_GroupBox.Controls.Add(this.listView1);
-            this.Agenda_GroupBox.Controls.Add(this.vScrollBar1);
-            this.Agenda_GroupBox.Location = new System.Drawing.Point(228, 4);
-            this.Agenda_GroupBox.Name = "Agenda_GroupBox";
-            this.Agenda_GroupBox.Size = new System.Drawing.Size(400, 506);
-            this.Agenda_GroupBox.TabIndex = 1;
-            this.Agenda_GroupBox.TabStop = false;
-            this.Agenda_GroupBox.Text = "Agenda";
+            this.EventNameTextBox.Location = new System.Drawing.Point(11, 46);
+            this.EventNameTextBox.Name = "EventNameTextBox";
+            this.EventNameTextBox.Size = new System.Drawing.Size(198, 20);
+            this.EventNameTextBox.TabIndex = 12;
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(124, 280);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(75, 23);
+            this.CreateButton.TabIndex = 10;
+            this.CreateButton.Text = "CREATE";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // EventDetailsLabel
+            // 
+            this.EventDetailsLabel.AutoSize = true;
+            this.EventDetailsLabel.Location = new System.Drawing.Point(8, 192);
+            this.EventDetailsLabel.Name = "EventDetailsLabel";
+            this.EventDetailsLabel.Size = new System.Drawing.Size(73, 13);
+            this.EventDetailsLabel.TabIndex = 8;
+            this.EventDetailsLabel.Text = "Event Details:";
+            // 
+            // EventLocationLabel
+            // 
+            this.EventLocationLabel.AutoSize = true;
+            this.EventLocationLabel.Location = new System.Drawing.Point(6, 151);
+            this.EventLocationLabel.Name = "EventLocationLabel";
+            this.EventLocationLabel.Size = new System.Drawing.Size(82, 13);
+            this.EventLocationLabel.TabIndex = 6;
+            this.EventLocationLabel.Text = "Event Location:";
             // 
             // EventNameLabel
             // 
@@ -89,21 +122,69 @@
             this.EventNameLabel.TabIndex = 0;
             this.EventNameLabel.Text = "Event Name:";
             // 
-            // EventNameRichTextBox
+            // EventLocationTextBox
             // 
-            this.EventNameRichTextBox.Location = new System.Drawing.Point(11, 46);
-            this.EventNameRichTextBox.Name = "EventNameRichTextBox";
-            this.EventNameRichTextBox.Size = new System.Drawing.Size(200, 21);
-            this.EventNameRichTextBox.TabIndex = 1;
-            this.EventNameRichTextBox.Text = "";
+            this.EventLocationTextBox.Location = new System.Drawing.Point(11, 168);
+            this.EventLocationTextBox.Name = "EventLocationTextBox";
+            this.EventLocationTextBox.Size = new System.Drawing.Size(195, 20);
+            this.EventLocationTextBox.TabIndex = 15;
             // 
-            // EventDateRichTextBox
+            // EventDetailsTextBox
             // 
-            this.EventDateRichTextBox.Location = new System.Drawing.Point(11, 87);
-            this.EventDateRichTextBox.Name = "EventDateRichTextBox";
-            this.EventDateRichTextBox.Size = new System.Drawing.Size(200, 21);
-            this.EventDateRichTextBox.TabIndex = 3;
-            this.EventDateRichTextBox.Text = "";
+            this.EventDetailsTextBox.Location = new System.Drawing.Point(11, 209);
+            this.EventDetailsTextBox.Name = "EventDetailsTextBox";
+            this.EventDetailsTextBox.Size = new System.Drawing.Size(198, 20);
+            this.EventDetailsTextBox.TabIndex = 16;
+            // 
+            // AlarmTimeLabel
+            // 
+            this.AlarmTimeLabel.AutoSize = true;
+            this.AlarmTimeLabel.Location = new System.Drawing.Point(11, 236);
+            this.AlarmTimeLabel.Name = "AlarmTimeLabel";
+            this.AlarmTimeLabel.Size = new System.Drawing.Size(62, 13);
+            this.AlarmTimeLabel.TabIndex = 17;
+            this.AlarmTimeLabel.Text = "Alarm Time:";
+            // 
+            // AlarmTimeTextBox
+            // 
+            this.AlarmTimeTextBox.Location = new System.Drawing.Point(14, 253);
+            this.AlarmTimeTextBox.Name = "AlarmTimeTextBox";
+            this.AlarmTimeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AlarmTimeTextBox.TabIndex = 18;
+            // 
+            // MinuteBeforeLabel
+            // 
+            this.MinuteBeforeLabel.AutoSize = true;
+            this.MinuteBeforeLabel.Location = new System.Drawing.Point(121, 253);
+            this.MinuteBeforeLabel.Name = "MinuteBeforeLabel";
+            this.MinuteBeforeLabel.Size = new System.Drawing.Size(78, 13);
+            this.MinuteBeforeLabel.TabIndex = 19;
+            this.MinuteBeforeLabel.Text = "Minutes Before";
+            // 
+            // AgendaDatePicker
+            // 
+            this.AgendaDatePicker.CustomFormat = "yyyy.MM.dd HH:mm";
+            this.AgendaDatePicker.Location = new System.Drawing.Point(9, 88);
+            this.AgendaDatePicker.Name = "AgendaDatePicker";
+            this.AgendaDatePicker.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.AgendaDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.AgendaDatePicker.TabIndex = 20;
+            // 
+            // EventHourTextBox
+            // 
+            this.EventHourTextBox.Location = new System.Drawing.Point(11, 127);
+            this.EventHourTextBox.Name = "EventHourTextBox";
+            this.EventHourTextBox.Size = new System.Drawing.Size(23, 20);
+            this.EventHourTextBox.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Event Time: (24:00)";
             // 
             // EventDateLabel
             // 
@@ -114,103 +195,48 @@
             this.EventDateLabel.TabIndex = 2;
             this.EventDateLabel.Text = "Event Date:";
             // 
-            // EventTimeRichTextBox
+            // label1
             // 
-            this.EventTimeRichTextBox.Location = new System.Drawing.Point(11, 128);
-            this.EventTimeRichTextBox.Name = "EventTimeRichTextBox";
-            this.EventTimeRichTextBox.Size = new System.Drawing.Size(200, 21);
-            this.EventTimeRichTextBox.TabIndex = 5;
-            this.EventTimeRichTextBox.Text = "";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = ":";
             // 
-            // label3
+            // EventMinTextBox
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 111);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Event Time:";
+            this.EventMinTextBox.Location = new System.Drawing.Point(56, 128);
+            this.EventMinTextBox.Name = "EventMinTextBox";
+            this.EventMinTextBox.Size = new System.Drawing.Size(25, 20);
+            this.EventMinTextBox.TabIndex = 22;
             // 
-            // EventLocationRichTextBox
+            // label2
             // 
-            this.EventLocationRichTextBox.Location = new System.Drawing.Point(9, 169);
-            this.EventLocationRichTextBox.Name = "EventLocationRichTextBox";
-            this.EventLocationRichTextBox.Size = new System.Drawing.Size(200, 21);
-            this.EventLocationRichTextBox.TabIndex = 7;
-            this.EventLocationRichTextBox.Text = "";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = ".";
             // 
-            // EventLocationLabel
+            // EventSecondTextBox
             // 
-            this.EventLocationLabel.AutoSize = true;
-            this.EventLocationLabel.Location = new System.Drawing.Point(6, 152);
-            this.EventLocationLabel.Name = "EventLocationLabel";
-            this.EventLocationLabel.Size = new System.Drawing.Size(82, 13);
-            this.EventLocationLabel.TabIndex = 6;
-            this.EventLocationLabel.Text = "Event Location:";
-            // 
-            // EventDetailsRichTextBox
-            // 
-            this.EventDetailsRichTextBox.Location = new System.Drawing.Point(11, 210);
-            this.EventDetailsRichTextBox.Name = "EventDetailsRichTextBox";
-            this.EventDetailsRichTextBox.Size = new System.Drawing.Size(200, 187);
-            this.EventDetailsRichTextBox.TabIndex = 9;
-            this.EventDetailsRichTextBox.Text = "";
-            // 
-            // EventDetailsLabel
-            // 
-            this.EventDetailsLabel.AutoSize = true;
-            this.EventDetailsLabel.Location = new System.Drawing.Point(8, 193);
-            this.EventDetailsLabel.Name = "EventDetailsLabel";
-            this.EventDetailsLabel.Size = new System.Drawing.Size(73, 13);
-            this.EventDetailsLabel.TabIndex = 8;
-            this.EventDetailsLabel.Text = "Event Details:";
-            // 
-            // CreateButton
-            // 
-            this.CreateButton.Location = new System.Drawing.Point(136, 426);
-            this.CreateButton.Name = "CreateButton";
-            this.CreateButton.Size = new System.Drawing.Size(75, 23);
-            this.CreateButton.TabIndex = 10;
-            this.CreateButton.Text = "CREATE";
-            this.CreateButton.UseVisualStyleBackColor = true;
-            // 
-            // vScrollBar1
-            // 
-            this.vScrollBar1.Location = new System.Drawing.Point(383, 9);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(17, 494);
-            this.vScrollBar1.TabIndex = 0;
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(22, 29);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(348, 471);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // AlarmCheckBox
-            // 
-            this.AlarmCheckBox.AutoSize = true;
-            this.AlarmCheckBox.Location = new System.Drawing.Point(11, 403);
-            this.AlarmCheckBox.Name = "AlarmCheckBox";
-            this.AlarmCheckBox.Size = new System.Drawing.Size(52, 17);
-            this.AlarmCheckBox.TabIndex = 11;
-            this.AlarmCheckBox.Text = "Alarm";
-            this.AlarmCheckBox.UseVisualStyleBackColor = true;
+            this.EventSecondTextBox.Location = new System.Drawing.Point(103, 127);
+            this.EventSecondTextBox.Name = "EventSecondTextBox";
+            this.EventSecondTextBox.Size = new System.Drawing.Size(24, 20);
+            this.EventSecondTextBox.TabIndex = 24;
             // 
             // TaskWarrior
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 522);
-            this.Controls.Add(this.Agenda_GroupBox);
+            this.ClientSize = new System.Drawing.Size(228, 522);
             this.Controls.Add(this.EventCreation_GroupBox);
             this.Name = "TaskWarrior";
             this.Text = "TASK-WARRIOR!";
             this.EventCreation_GroupBox.ResumeLayout(false);
             this.EventCreation_GroupBox.PerformLayout();
-            this.Agenda_GroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -218,21 +244,24 @@
         #endregion
 
         private System.Windows.Forms.GroupBox EventCreation_GroupBox;
-        private System.Windows.Forms.GroupBox Agenda_GroupBox;
         private System.Windows.Forms.Button CreateButton;
-        private System.Windows.Forms.RichTextBox EventDetailsRichTextBox;
         private System.Windows.Forms.Label EventDetailsLabel;
-        private System.Windows.Forms.RichTextBox EventLocationRichTextBox;
         private System.Windows.Forms.Label EventLocationLabel;
-        private System.Windows.Forms.RichTextBox EventTimeRichTextBox;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox EventDateRichTextBox;
-        private System.Windows.Forms.Label EventDateLabel;
-        private System.Windows.Forms.RichTextBox EventNameRichTextBox;
         private System.Windows.Forms.Label EventNameLabel;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
-        private System.Windows.Forms.CheckBox AlarmCheckBox;
+        private System.Windows.Forms.TextBox EventNameTextBox;
+        private System.Windows.Forms.TextBox EventDetailsTextBox;
+        private System.Windows.Forms.TextBox EventLocationTextBox;
+        private System.Windows.Forms.TextBox AlarmTimeTextBox;
+        private System.Windows.Forms.Label AlarmTimeLabel;
+        private System.Windows.Forms.Label MinuteBeforeLabel;
+        private System.Windows.Forms.DateTimePicker AgendaDatePicker;
+        private System.Windows.Forms.TextBox EventHourTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label EventDateLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EventMinTextBox;
+        private System.Windows.Forms.TextBox EventSecondTextBox;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Agenda/Agenda/Form1.cs
+++ Agenda/Agenda/Form1.cs
@@ -8,13 +8,66 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Agenda
 {
     public partial class TaskWarrior : Form
     {
+
         public TaskWarrior()
         {
             InitializeComponent();
         }
-    }
+
+        private void EventCreation_GroupBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            string sEventName;
+            DateTime dtEventDate;
+            DateTime dtEventDateClick;
+            string sEventHour;
+            string sEventMinute;
+            string sEventSecond;
+            string sEventLocation;
+            string sEventDetails;
+            string sAlarmTime;
+            int iEventHour;
+            int iEventMinute;
+            int iEventSecond;
+
+            sEventName = EventNameTextBox.Text;
+            dtEventDateClick = AgendaDatePicker.Value;
+            sEventHour = EventHourTextBox.Text;
+            sEventMinute = EventMinTextBox.Text;
+            sEventSecond = EventSecondTextBox.Text;
+            sEventLocation = EventLocationTextBox.Text;
+            sEventDetails = EventDetailsTextBox.Text;
+            sAlarmTime = AlarmTimeTextBox.Text;
+            iEventHour = Convert.ToInt32(sEventHour);
+            iEventMinute = Convert.ToInt32(sEventMinute);
+            iEventSecond = Convert.ToInt32(sEventSecond);
+            dtEventDate = new DateTime(dtEventDateClick.Year, dtEventDateClick.Month,
+                dtEventDateClick.Day, iEventHour, iEventMinute, iEventSecond);
+
+            System.Console.WriteLine(sEventName + " " + dtEventDate + " " +
+                sEventLocation + " " + sEventDetails + " " + sAlarmTime);
+            //sEventName = Name of event
+            //dtEventDate = Datetime of event
+            //sEventLocation = Event Location
+            //sEventDetails = Extra info
+            //sAlarmTime is how far ahead they want the alarm sounded
+            //These all are the variables that the info the user entered is saved into when they hit create
+
+          EventNameTextBox.Text = "";
+          EventHourTextBox.Text = "";
+          EventLocationTextBox.Text = "";
+          EventDetailsTextBox.Text = "";
+          AlarmTimeTextBox.Text = "";
+        }
+
+    }    
 }
